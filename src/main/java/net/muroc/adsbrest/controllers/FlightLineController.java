3f5085115a278a114a0fefb78e190c431cfaf0bb
brendan.dwyer <bdwyer@novettta.com>
2020-02-10 13:23:07 -0500

    Updated Controllers.  Line controller working.  Point controller needs to test for line before inserting record.

--- src/main/java/net/muroc/adsbrest/controllers/FlightLineController.java
+++ src/main/java/net/muroc/adsbrest/controllers/FlightLineController.java
@@ -1,4 +1,45 @@
 package net.muroc.adsbrest.controllers;
 
-public class FlightLineController {
+import net.muroc.adsbrest.domain.FlightLine;
+import net.muroc.adsbrest.repositories.FlightLineRepository;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import net.muroc.adsbrest.services.FlightLineService;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path="/flightline")
+public class FlightLineController
+{
+    @Autowired
+    private FlightLineService flightLineService;
+
+    @GetMapping("flights/{flight_id}/flightline")
+    public FlightLine retrieveFlightLines(@PathVariable int flight_id)
+    {
+        return flightLineService.getFlightLineById(flight_id);
+    }
+
+    @GetMapping("flights")
+    public Iterable<FlightLine> retrieveAllFlightLines()
+    {
+        return flightLineService.listAllFlightLines();
+    }
+
+    @PostMapping("flights/new")
+    public FlightLine saveNewFlightLine(@RequestBody FlightLine flightLine)
+    {
+        return flightLineService.saveFlightLine(flightLine);
+    }
 }
--- src/main/java/net/muroc/adsbrest/controllers/FlightPointController.java
+++ src/main/java/net/muroc/adsbrest/controllers/FlightPointController.java
@@ -2,12 +2,12 @@ package net.muroc.adsbrest.controllers;
 
 import net.muroc.adsbrest.domain.FlightPoint;
 import net.muroc.adsbrest.repositories.FlightPointRepository;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @RequestMapping(path="/flightpoint")
 public class FlightPointController
 {
@@ -18,5 +18,18 @@ public class FlightPointController
     public @ResponseBody Iterable<FlightPoint> getFlightPoints()
     {
         return flightPointRepository.findAll();
+        //return flightPointRepository.count();
+    }
+
+    @GetMapping(path="/count")
+    public @ResponseBody long getFlightPointCount()
+    {
+        return flightPointRepository.count();
+    }
+
+    @PostMapping(path="/points/new")
+    public FlightPoint saveNewFlightLine(@RequestBody FlightPoint flightPoint)
+    {
+        return flightPointRepository.save(flightPoint);
     }
 }
